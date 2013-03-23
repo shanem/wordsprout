a1906f5e932349c31f0d59dac11b8fda1599602d
Shane Mooney <smmoonshoes@gmail.com>
2013-03-23 10:29:08 -0700

    Stubbed in main two activities.

--- src/com/lauren/wordsprout/IdentifyActivity.java
+++ src/com/lauren/wordsprout/IdentifyActivity.java
@@ -3,26 +3,20 @@ package com.lauren.wordsprout;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 
-public class MainActivity extends Activity {
+public class IdentifyActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_identify);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
-    }
-    
-    public void identify(View view) {
-    	new AlertDialog.Builder(this).setMessage("Identify!").show();
-    	startActivity(new Intent(this, IdentifyActivity.class));
     }
 }
