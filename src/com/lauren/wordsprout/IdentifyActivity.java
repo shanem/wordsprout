62b2c2faf6298e2c04ad5996b904206340f6f532
Shane Mooney <smmoonshoes@gmail.com>
2013-03-23 12:00:22 -0700

    Added low-res photo capture tied to each question.

--- src/com/lauren/wordsprout/IdentifyActivity.java
+++ src/com/lauren/wordsprout/IdentifyActivity.java
@@ -1,14 +1,27 @@
 package com.lauren.wordsprout;
 
-import com.lauren.wordsprout.data.QuestionSets;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Date;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.preference.PreferenceManager;
+import android.provider.MediaStore.Images.Media;
 import android.view.Menu;
 import android.view.View;
-import android.widget.RelativeLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.lauren.wordsprout.data.QuestionSets;
 
 public class IdentifyActivity extends Activity {
 
@@ -18,12 +31,16 @@ public class IdentifyActivity extends Activity {
 	private String[] questions;
 	
 	private TextView questionView;
+	private ImageView imageView;
+	
+	private final int FROM_CAMERA = 1;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_identify);
         questionView = (TextView) findViewById(R.id.question_text);
+        imageView = (ImageView) findViewById(R.id.answer_photo);
 
         
         currentQuestionIndex = 0;
@@ -39,7 +56,13 @@ public class IdentifyActivity extends Activity {
         		questions = QuestionSets.bodyQuestions;
         		break;
         }
-        questionView.setText(questions[currentQuestionIndex]);
+    }
+    
+    @Override
+    public void onResume() {
+    	super.onResume();
+    	questionView.setText(questions[currentQuestionIndex]);
+    	trySetPhoto();
     }
 
     @Override
@@ -56,5 +79,58 @@ public class IdentifyActivity extends Activity {
     		currentQuestionIndex = 0;
     	}
     	questionView.setText(questions[currentQuestionIndex]);
+    	trySetPhoto();
+    }
+    
+    public void takePhoto(View view) {
+		Intent i = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+		ContentValues values = new ContentValues();
+		startActivityForResult(i, FROM_CAMERA);
+    }
+    
+    public void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+        if (resultCode == Activity.RESULT_CANCELED) {
+            return;
+        }
+
+        Intent intent;
+        switch (reqCode) {
+            case FROM_CAMERA:
+            	Bitmap photo = (Bitmap) data.getExtras().get("data");
+            	imageView.setImageBitmap(photo);
+            	
+//            	SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+//                SharedPreferences.Editor editor = preferences.edit();
+//                editor.put(PreferenceConstants.LAST_NOTIFIED, new Date().getTime());
+//                editor.commit();
+                
+                try {
+                	FileOutputStream out = new FileOutputStream(getCurrentImagePath());
+                	photo.compress(Bitmap.CompressFormat.PNG, 90, out);
+                }
+                catch (FileNotFoundException e) {
+                	throw new RuntimeException(e);
+                }
+                break;
+        }
+    }
+    
+    private void trySetPhoto() {
+    	File imageFile = new  File(getCurrentImagePath());
+    	if (imageFile.exists()){
+    	    Bitmap bitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
+    	    imageView.setImageBitmap(bitmap);
+    	    imageView.setVisibility(View.VISIBLE);
+    	}
+    	else {
+    		imageView.setVisibility(View.INVISIBLE);
+    	}
+    }
+    
+    private String getCurrentImagePath() {
+    	String path = Environment.getExternalStorageDirectory().toString();
+        path += "/WordSprout-image-" + currentQuestionCategory + "_" + currentQuestionIndex + ".jpg";
+        return path;
     }
 }
