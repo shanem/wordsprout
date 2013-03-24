0b9e6d5f6d2b04615ae62ffdc84c1df214d0d77e
emmychen <echen@abitalk.com>
2013-03-24 11:42:03 -0700

    screen shot sharing

--- src/com/lauren/wordsprout/ImageActivity.java
+++ src/com/lauren/wordsprout/ImageActivity.java
@@ -8,11 +8,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore.Images;
 import android.view.View;
-import android.widget.Button;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class ImageActivity extends Activity {
@@ -28,13 +30,28 @@ public class ImageActivity extends Activity {
 		imageViews = new ArrayList<ImageView>();
 		imageViews.add((ImageView) findViewById(R.id.img01));
 		imageViews.add((ImageView) findViewById(R.id.img02));
+		
+		
 		sharedBtn = (Button) findViewById(R.id.sharedBtn);
 		sharedBtn.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
+				View view =  findViewById(android.R.id.content).getRootView();
+				view.setDrawingCacheEnabled(true);
+				BitmapFactory.Options options = new BitmapFactory.Options();
+	            options.inSampleSize = 4;
+				Bitmap bitmap = Bitmap.createBitmap(view.getDrawingCache());
+				String path = Images.Media.insertImage(getContentResolver(), bitmap, "Image to Share", null);
+				Uri sharedUri = Uri.parse(path);
+				
 				Intent sharedIntent = new Intent(Intent.ACTION_SEND);
+				//sharedIntent.setType("text/html");
+				//sharedIntent.putExtra(android.content.Intent.EXTRA_TEXT, Html.fromHtml("<p>This is the text that will be shared.</p>"));
+				
+				sharedIntent.setType("image/png");
+				sharedIntent.putExtra(Intent.EXTRA_STREAM, sharedUri);
 				startActivity(Intent.createChooser(sharedIntent, "Share the pictures using..."));
 			}
 		});
@@ -57,7 +74,7 @@ public class ImageActivity extends Activity {
     	    //		, iv.getDrawable().getBounds().height()/2);
     	    
     	    iv.setImageBitmap(bitmap);
-    	   iv.setPivotX(iv.getWidth()/2);
+    	    iv.setPivotX(iv.getWidth()/2);
     	   // iv.setPivotX(0);
     	   // iv.setPivotY(0);
     	    iv.setPivotY(iv.getHeight()/2);
